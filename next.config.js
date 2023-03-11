dc30ba223ae01d63a882d9593a34034168dc4087
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2023-03-11 18:19:35 +0100

    feat: init wishing tree project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['image.ibb.co', 'preview.ibb.co']
+  }
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<>
+    <Script src="https://code.jquery.com/jquery-2.2.4.min.js" />
+    <Script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" />
+    <Script src="/main.js" />
+    <Component {...pageProps} />
+  </>
+  )
 }
