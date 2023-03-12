a01d1e243be178353464cd384f66d8ba1e447137
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2023-03-12 12:39:44 +0100

    fix: move jquery script to document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,6 @@ import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (<>
-    <Script src="https://code.jquery.com/jquery-2.2.4.min.js" />
-    <Script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" />
     <Script src="/main.js" />
     <Component {...pageProps} />
   </>
