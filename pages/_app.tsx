313a266ccc4bfbbd5c139c9599a372d0aa03457a
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2023-03-12 12:41:48 +0100

    fix: move cdn bootstrap to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }: AppProps) {
   return (<>
     <Script src="/main.js" />
+    <Script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" />
     <Component {...pageProps} />
   </>
   )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
         <meta charSet="utf-8" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossOrigin="anonymous"/>
         <Script src="https://code.jquery.com/jquery-2.2.4.min.js" strategy="beforeInteractive" />
-        <Script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" />
       </Head>
       <body>
         <Main />
